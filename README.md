76bae2923104a555e0600cdf10632b06fa77239f
Christopher Beards <beardscm@gmail.com>
2018-09-02 18:57:00 -0400

    Search; Created

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A simple React application built with the Context API to find song lyrics.
 
+## Resources
+
+[Musix Match API](https://developer.musixmatch.com/)
+
 ## Author
 
 [Christopher Beards](https://www.linkedin.com/in/christopher-beards-1292b529/)
--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import { Provider } from './context';
+
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
-import { Provider } from './context';
+import Lyrics from './components/tracks/Lyrics';
 
 class App extends Component {
   render() {
@@ -16,6 +18,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
               </Switch>
             </div>
           </React.Fragment>
