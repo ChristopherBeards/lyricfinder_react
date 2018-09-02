7804d4eff36bf242c53fb82dd266da60ab3ac09c
Christopher Beards <beardscm@gmail.com>
2018-09-02 15:11:55 -0400

    Added env. Axios; initial implmentation

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 // Create the context
 const Context = React.createContext();
@@ -11,6 +12,17 @@ export class Provider extends Component {
     ],
     heading: 'Top 10 Tracks',
   };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+          process.env.REACT_APP_MM_KEY
+        }`,
+      )
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  }
 
   render() {
     return (
