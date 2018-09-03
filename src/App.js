aa322daf612125ec1978df3aece48dd5f4519fcd
Christopher Beards <beardscm@gmail.com>
2018-09-02 23:50:11 -0400

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import { Provider } from './context';
 
 import Navbar from './components/layout/Navbar';
-import Index from './components/layout/Index';
+import Indexx from './components/layout/Index';
 import Lyrics from './components/tracks/Lyrics';
 
 class App extends Component {
@@ -17,7 +17,7 @@ class App extends Component {
             <Navbar />
             <div className="container">
               <Switch>
-                <Route exact path="/" component={Index} />
+                <Route exact path="/" component={Indexx} />
                 <Route exact path="/lyrics/track/:id" component={Lyrics} />
               </Switch>
             </div>
